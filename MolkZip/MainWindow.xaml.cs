1fcb0c98a1bf4c5e75d4a90f9117783f5134248a
Farhad Tavakoli <72437846+Farhadtavakoli@users.noreply.github.com>
2021-10-22 15:48:57 +0200

    Add buttons for Add and Remove

--- MolkZip/MainWindow.xaml.cs
+++ MolkZip/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MolkZip
 {
@@ -30,6 +31,7 @@ namespace MolkZip
 
         private void Files_Drop(object sender, DragEventArgs e)
         {
+            labelTip.Visibility = Visibility.Hidden;
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
@@ -43,11 +45,34 @@ namespace MolkZip
                         ListBoxItem file = new ListBoxItem();
                         file.Content = filename;
                         FilesList.Items.Add(file);
+                        
                     }                    
                     
                 }
             }
         }
-     
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            OpenFileDialog newDiolog = new OpenFileDialog();
+            newDiolog.ShowDialog();
+            string fileName = newDiolog.FileName;
+            FilesList.Items.Add(fileName);
+            labelTip.Visibility = Visibility.Hidden;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            for (int i = FilesList.SelectedItems.Count-1 ; i >= 0; i--)
+            {
+               
+                FilesList.Items.Remove(FilesList.SelectedItems[i]);
+               
+            }
+                     
+        }
     }
 }
