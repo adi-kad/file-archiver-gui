fa2194115a8a142986239f071947b10830034b52
Adnan Kadrispahic <70524344+adi-kad@users.noreply.github.com>
2021-10-26 13:21:11 +0200

    Revert "Add remove files"

--- MolkZip/MainWindow.xaml.cs
+++ MolkZip/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Win32;
 
 namespace MolkZip
 {
@@ -31,16 +30,10 @@ namespace MolkZip
         public MainWindow()
         {
             InitializeComponent();
-            
-           
         }
 
         private void Files_Drop(object sender, DragEventArgs e)
         {
-            
-            labelTip.Visibility = Visibility.Hidden;
-            remove.Visibility = Visibility.Visible;
-           
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
@@ -54,116 +47,12 @@ namespace MolkZip
                         ListBoxItem file = new ListBoxItem();
                         file.Content = filename;
                         FilesList.Items.Add(file);
-
-                        
-                    }                    
-                    
-
                     }
 
-
                 }
             }
         }
 
-
-        private void browse(object sender, RoutedEventArgs e)
-        {
-            addFiles();
-        }
-
-        private void removeFile(object sender, RoutedEventArgs e)
-        {
-                FilesList.Items.Remove(FilesList.SelectedItem);
-                hideRemoveButton();
-
-        }
-
-        private void FilesList_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Delete)
-            {
-               
-                FilesList.Items.Remove(FilesList.SelectedItem);
-                hideRemoveButton();
-            }
-        }
-
-        private void mainWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-            {
-                if (MessageBox.Show("Do you want to close this window?",
-                    "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-                {
-                    mainWindow.Close(); 
-                }
-                else
-                {
-                    // Do not close the window  
-                }
-            }
-            else if (e.Key == Key.Insert)
-            {
-                addFiles();
-            }
-        }
-
-        private void hideRemoveButton()
-        {
-            if (FilesList.Items.Count == 0)
-            {
-                remove.Visibility = Visibility.Hidden;
-                labelTip.Visibility = Visibility.Visible;
-            }
-        }
-        private void addFiles()
-        {
-
-            OpenFileDialog newDiolog = new OpenFileDialog();
-            newDiolog.ShowDialog();
-            string fileName = newDiolog.FileName;
-            if (fileName != "")
-            {
-                FilesList.Items.Add(fileName);
-                labelTip.Visibility = Visibility.Hidden;
-            }
-           
-            if (FilesList.Items.Count > 0)
-            {
-                remove.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void addFile_MouseMove(object sender, MouseEventArgs e)
-        {
-            addFile.Background = Brushes.Magenta;
-        }
-
-        private void addFile_MouseEnter(object sender, MouseEventArgs e)
-        {
-            addFile.FontSize = 18;
-            addFile.FontWeight = FontWeights.Bold;
-        }
-
-        private void addFile_MouseLeave(object sender, MouseEventArgs e)
-        {
-            addFile.FontSize = 16;
-            addFile.FontWeight = FontWeights.Normal;
-        }
-
-        private void remove_MouseEnter(object sender, MouseEventArgs e)
-        {
-            remove.FontSize = 18;
-            remove.FontWeight = FontWeights.Bold;
-        }
-
-        private void remove_MouseLeave(object sender, MouseEventArgs e)
-        {
-            remove.FontSize = 16;
-            remove.FontWeight = FontWeights.Bold;
-
-         }
         public static void RunCLIprogram(string programPath, List<string> args)
         {
             //Combine list elements into a single string, where each
@@ -197,7 +86,6 @@ namespace MolkZip
             args.Add(sourceFilePath);
             args.Add("-d testFolder");
             RunCLIprogram(unmolkPath, args);
-
         }
-    
+    }
 }
